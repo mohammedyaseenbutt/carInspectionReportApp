335401981ab8050c80341fd9a16429ced738c084
Saniya Tariq <79018100+saniya384@users.noreply.github.com>
2023-02-24 17:08:35 +0500

    eject

--- android/app/src/main/jni/MainComponentsRegistry.h
+++ android/app/src/main/jni/MainComponentsRegistry.h
@@ -13,7 +13,7 @@ class MainComponentsRegistry
  public:
   // Adapt it to the package you used for your Java class.
   constexpr static auto kJavaDescriptor =
-      "Lcom/ordertaking/newarchitecture/components/MainComponentsRegistry;";
+      "Lcom/atoz/newarchitecture/components/MainComponentsRegistry;";
 
   static void registerNatives();
 
